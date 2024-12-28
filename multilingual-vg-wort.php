05228e7420fd3beab43bf3b03252c48d93c55dbf
Bernhard Kau <github@kau-boys.de>
2024-12-29 00:13:35 +0200

    Add "GitHub Plugin URI" to plugin header comment

--- multilingual-vg-wort.php
+++ multilingual-vg-wort.php
@@ -3,6 +3,7 @@
  * Plugin Name: Multilingual VG WORT
  * Description: Adds a VG WORT pixel from the German site to connected sites in other languages.
  * Requires Plugins: multilingualpress, wp-worthy
+ * GitHub Plugin URI: 2ndkauboy/multilingual-vg-wort
  */
 
 function multilingual_vg_wort_footer() {
